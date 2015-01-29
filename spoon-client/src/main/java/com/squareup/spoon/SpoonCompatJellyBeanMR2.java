857eb223b7f1fbb203479ff7a24b1b9677271496
Scott Weber <scott@mobiata.com>
2015-01-29 12:07:34 -0500

    update to android-maven-plugin 4.0.0
    This update required moving some files around and also using
    maven-android-sdk-deployer to create local android.jar files
    that can be consumed by maven, rather than relying on the
    outdated and no longer supported android packages on maven
    central.

---
+++ spoon-client/src/main/java/com/squareup/spoon/SpoonCompatJellyBeanMR2.java
@@ -0,0 +1,39 @@
+package com.squareup.spoon;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import android.annotation.TargetApi;
+import android.app.Instrumentation;
+import android.graphics.Bitmap;
+import android.os.Build;
+
+import static android.graphics.Bitmap.CompressFormat.PNG;
+import static com.squareup.spoon.Chmod.chmodPlusR;
+
+public class SpoonCompatJellyBeanMR2 {
+
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+	protected static void takeScreenshot(Instrumentation instrumentation, File file) throws IOException {
+		final Bitmap bitmap = instrumentation.getUiAutomation().takeScreenshot();
+
+		OutputStream fos = null;
+		try {
+			fos = new BufferedOutputStream(new FileOutputStream(file));
+			bitmap.compress(PNG, 100 /* quality */, fos);
+
+			chmodPlusR(file);
+		}
+		finally {
+			bitmap.recycle();
+			if (fos != null) {
+				fos.close();
+			}
+		}
+
+	}
+
+}
